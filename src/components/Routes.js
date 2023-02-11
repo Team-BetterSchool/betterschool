3939f45b20c60dab00e9d374aa03d7e39d5e99b2
Valerie Jones <4821024+vljones133@users.noreply.github.com>
2023-02-11 16:33:36 -0600

    refactored Copyright component, added ErrorPage, refactored listItems, added ParentSignUp template component, added Routes componenent shell

---
+++ src/components/Routes.js
@@ -0,0 +1,15 @@
+// import ErrorPage from './ErrorPage';
+
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <Dashboard />,
+//     errorElement: <ErrorPage />,
+//     children: [
+//       {
+//         path: 'students/:studentId',
+//         element: <Student />,
+//       },
+//     ],
+//   },
+// ]);
