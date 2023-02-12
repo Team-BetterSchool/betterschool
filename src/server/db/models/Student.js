249ca3f9505e9048c2276820ff2c71d39550ed1a
Fiona Murray <fiona.c.murray@gmail.com>
2023-02-12 17:09:52 -0500

    two schemas made

---
+++ src/server/db/models/Student.js
@@ -0,0 +1,56 @@
+import mongoose from 'mongoose';
+
+const StudentSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 50,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 50,
+    },
+    username: {
+      type: String,
+      required: true,
+      max: 50,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 5,
+    },
+    gradeLevel: {
+      type: Number,
+      required: true,
+    },
+    birthday: {
+      type: Date,
+      required: true,
+    },
+    parent1_name: {
+      type: String,
+      required: true,
+    },
+    parent1_phone: {
+      type: Number,
+      required: true,
+    },
+    parent2_name: {
+      type: String,
+    },
+    parent2_phone: {
+      type: Number,
+    },
+  },
+  { timestamps: true }
+);
+
+const Student = mongoose.model('Student', StudentSchema);
+
+export default Student;
---
+++ src/server/db/models/Teacher.js
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose';
+
+const TeacherSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 50,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 50,
+    },
+    username: {
+      type: String,
+      required: true,
+      max: 50,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 5,
+    },
+  },
+  { timestamps: true }
+);
+
+const Teacher = mongoose.model('Teacher', TeacherSchema);
+
+export default Teacher;
